4b5b8d135572fa4175d7b3a38085af5b3f50c049
JonAguinaga <jonaguinaga1@gmail.com>
2018-05-07 20:08:05 +0200

    Beta phase of the game

--- Ejercicio3-Ahorcado.py
+++ Ejercicio3-Ahorcado.py
@@ -1,3 +1,5 @@
+print("Un momento, el programa se está cargando")
+
 import requests
 import random
 
@@ -13,32 +15,35 @@ respuesta = list(respuestabyte.decode("utf-8"))
 
 contador = 5
 
-#letra = input()
-
 inprogress = list()
 
 for char in respuesta:
     inprogress.append("_")
 
-print(respuesta)
-print(inprogress)
-#
-# while inprogress != response and contador != 0:
-#     print("Hola, introduce tu letra")
-#
-#     respuesta = list("Hoz")
-#
-#     if letra in respuesta:
-#         for letra2 in range (0,len(respuesta)):
-#             if letra == respuesta[letra2]:
-#                 inprogress[letra2]=letra
-#         print("Enhorabuena, la letra es correcta")
-#     else:
-#         contador = contador-1
-#         print("Esa letra no está en la palabra")
-#     print("""Te quedan 'contador' intentos
-#             Tu palabra es 'inprogress'""")
-#
-#
-#
-# pass
+#print(respuesta)
+#print(inprogress)
+
+while inprogress != respuesta and contador != 0:
+
+    print("Introduce una letra")
+
+    letra = input()
+
+    if letra in respuesta:
+        for letra2 in range (0,len(respuesta)):
+            if letra == respuesta[letra2]:
+                inprogress[letra2]=letra
+        print("Enhorabuena, la letra es correcta")
+    else:
+        contador = contador-1
+        print("Esa letra no está en la palabra")
+    print("Te quedan", contador, """intentos
+            Tu palabra es""", " ".join(inprogress))
+
+pass
+
+if inprogress == respuesta:
+    print("Enhorabuena. Tu palabra es", "".join(respuesta))
+
+else:
+    print("Lo sentimos, tu palabra era", "".join(respuesta))
